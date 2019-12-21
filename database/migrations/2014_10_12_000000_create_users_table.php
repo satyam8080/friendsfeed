464eb32a1f1e8d53cc300023806f74ab4d705041
Satyam <iamsatyam26@gmail.com>
2019-12-21 13:51:32 +0530

    DB name fixed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,16 +16,24 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('profileImage');
+            $table->string('profileCover');
+            $table->integer('following')->length(11);
+            $table->integer('followers')->length(11);
+            $table->string('bio');
+            $table->string('country');
+            $table->string('website');
             $table->timestamps();
         });
     }
 
     /**
-     * Reverse the migrations.
+     * Reverse the migrations. 
      *
      * @return void
      */
